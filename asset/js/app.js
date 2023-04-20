cc46fd79e0736413d43f06418e345b54cf96ba97
unknown <maremcoGodbless55@gmail.com>
2023-04-20 13:14:40 +0100

    more users in localStorage

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,34 +1,52 @@
 const submit_button = document.querySelector(".button");
+
+// all input data received this variables
+const fname = document.getElementById("fname");
+const lname = document.getElementById("lname");
+const email = document.getElementById("email");
+const pass = document.getElementById("pass");
+const cpass = document.getElementById("cpass");
+
+const usersList = JSON.parse(localStorage.getItem("users")) || []
 // submit_button.onclick = () => {
+    // alert('hello world')
 submit_button.onclick = (e) => {
     e.preventDefault();
 
-    // all input data received this variables
-    const fname = document.getElementById("fname").value;
-    const lname = document.getElementById("lname").value;
-    const email = document.getElementById("email").value;
-    const pass = document.getElementById("pass").value;
-    const cpass = document.getElementById("cpass").value;
+    const user = {
+        id: usersList.length+1,
+        firstName : fname.value,
+        lastName : lname.value,
+        email : email.value,
+        password : pass.value
+
+    }
+    
 
     // now store this data in your web browser store
-    localStorage.setItem('FirstName', fname);
+   /* localStorage.setItem('FirstName', fname);
     localStorage.setItem('LastName', lname);
     localStorage.setItem('Email', email);
     localStorage.setItem('Password', pass);
     localStorage.setItem('Cpassword', cpass);
+    */
+
+    // window.localStorage.setItem("locateData", JSON.stringify(nameData))
 
     // now write some condition
-    if(fname == "" && lname == "" && email == "" && pass == "" && cpass == ""){
+    if(fname.value == "" || lname.value == "" || email.value == "" || pass.value == "" || cpass.value == ""){
         // let's change this alert system
         // alert("Input field has no value");
         swal("Opps..!", "Input field has no value!", "error");
     }
     else {
-        if(pass !== cpass){
+        if(pass.value !== cpass.value){
             // alert("password not matching");
             swal("Opps..!", "Password does not matching!", "error");
         }
         else{
+            usersList.push(user)
+    localStorage.setItem("users", JSON.stringify(usersList))
             // alert('registration successful');
             // swal("Good job!", "Registration successful", "success");
             swal({
@@ -54,19 +72,24 @@ login.onclick = (e) => {
     e.preventDefault();
 
     // cautch the value which is type user login page
-    const emailAddress = document.getElementById("Email").value;
-    const passWord = document.getElementById("pass").value;
+    const emailAddress = document.getElementById("login_email");
+    const passWord = document.getElementById("login_pass");
 
     // let's get value in localStorage which store user in registration form
-    const Email =  localStorage.getItem("Email");
-    const Password =  localStorage.getItem("Password");
+    // const Email =  localStorage.getItem("Email");
+    // const Password =  localStorage.getItem("Password");
 
+   console.log(emailAddress.value, passWord.value);
+    let user = usersList.find(user => user.email == emailAddress.value
+    )
+    console.log(user);
 
     if(emailAddress == "" && passWord == ""){
         swal("Opps..!", "Input field has no value!", "error");
     }
     else {
-        if(emailAddress == Email && passWord == Password){
+        if(emailAddress.value == user.email && passWord.value == user.password){
+
             // swal("Good job!", "Login successful", "success");
             swal({
                 title: "Good job!",
